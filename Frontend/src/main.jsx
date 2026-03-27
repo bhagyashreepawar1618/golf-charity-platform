af272fbb5602c534f14402a4f5f4f84082eec976
bhagyashreepawar1618 <bhagyashreepawar1618@gmail.com>
2026-03-28 00:54:25 +0530

    added routing for all

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,9 +1,36 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 import "./index.css";
+import Layout from "./components/main_components/Layout.jsx";
+import Home from "./components/main_components/Home.jsx";
+import Register from "./components/user_components/Register.jsx";
+import Login from "./components/user_components/Login.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "register",
+        element: <Register />,
+      },
+      {
+        path: "login",
+        element: <Login />,
+      },
+    ],
+  },
+]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}></RouterProvider>
   </StrictMode>,
 );
