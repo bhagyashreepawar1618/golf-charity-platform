24a476323dbeb7db18aa5669028a5932ea27ba50
bhagyashreepawar1618 <bhagyashreepawar1618@gmail.com>
2026-03-29 04:06:36 +0530

    added route for charitiy

--- server/src/routes/admin.routes.js
+++ server/src/routes/admin.routes.js
@@ -1,6 +1,7 @@
 import { Router } from 'express';
-import { registerAdmin, loginAdmin } from '../controllers/admin.controller.js';
+import { registerAdmin, loginAdmin, setCharity } from '../controllers/admin.controller.js';
 import { upload } from '../middlewares/multer.middleware.js';
+import { verifyJWT } from '../middlewares/adminauth.middleware.js';
 
 const router = Router();
 
@@ -15,5 +16,14 @@ router.route('/admin-register').post(
 );
 
 router.route('/login-admin').post(loginAdmin);
-
+router.route('/set-charity').post(
+  upload.fields([
+    {
+      name: 'image',
+      maxCount: 1,
+    },
+  ]),
+  verifyJWT,
+  setCharity
+);
 export default router;
