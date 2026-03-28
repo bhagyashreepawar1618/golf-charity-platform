4f6411ff2a18d483d95ba10014539a0b7fd803a4
bhagyashreepawar1618 <bhagyashreepawar1618@gmail.com>
2026-03-29 02:38:31 +0530

    created charity model to store charity info

---
+++ server/src/models/charity.model.js
@@ -0,0 +1,32 @@
+import mongoose, { Mongoose, Schema } from 'mongoose';
+
+const charitySchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    totalDonations: {
+      type: Number,
+      default: 0,
+    },
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Charity = mongoose.model('Charity', charitySchema);
