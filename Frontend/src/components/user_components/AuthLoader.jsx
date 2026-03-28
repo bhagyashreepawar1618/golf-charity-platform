4cbd253dd350cbb811d9433c9f052cf03991f27e
bhagyashreepawar1618 <bhagyashreepawar1618@gmail.com>
2026-03-28 16:29:51 +0530

    authLoader for getting user profile after refresh

---
+++ Frontend/src/components/user_components/AuthLoader.jsx
@@ -0,0 +1,34 @@
+import { useEffect } from "react";
+import axios from "axios";
+import { useUser } from "../../contexts/User.context.jsx";
+
+export default function AuthLoader({ children }) {
+  const { setUser } = useUser();
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const token = localStorage.getItem("UseraccessToken");
+
+        if (!token) return;
+
+        const res = await axios.get(
+          "http://localhost:8000/api/v1/user/get-current-user",
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          },
+        );
+
+        setUser(res.data.data);
+      } catch (error) {
+        console.log("Auto login failed");
+      }
+    };
+
+    fetchUser();
+  }, []);
+
+  return children;
+}
