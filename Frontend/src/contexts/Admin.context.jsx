2c41a498962652a1739365c4d2d31e71fc946444
bhagyashreepawar1618 <bhagyashreepawar1618@gmail.com>
2026-03-29 17:36:18 +0530

    added context to store all users

--- Frontend/src/contexts/Admin.context.jsx
+++ Frontend/src/contexts/Admin.context.jsx
@@ -3,9 +3,13 @@ import { useContext, useState, createContext } from "react";
 const AdminContext = createContext();
 export const AdminProvider = ({ children }) => {
   const [admin, setAdmin] = useState(null);
+  //to store all users list
+  const [users, setUsers] = useState([]);
   const value = {
     admin,
     setAdmin,
+    users,
+    setUsers,
   };
 
   return (
